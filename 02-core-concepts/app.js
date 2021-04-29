7d58a1f682bbb0fa01a1183b1d40145b85db7d82
Alexandre Ferreira <alexmlf93@gmail.com>
2021-04-29 11:46:57 -0300

    Handling events

--- 02-core-concepts/app.js
+++ 02-core-concepts/app.js
@@ -5,13 +5,25 @@ const app = Vue.createApp({
         link: 'https://vuejs.org/',
         goalTextA: 'Finish the course and learn Vue!',
         goalTextB: 'Master Vue and build amazing apps!'
-      }
+      },
+      counter: 0,
+      me: { name: '' }
     }
   },
   methods: {
     courseGoal() {
       let random = Math.random();
       return random < 0.5 ? this.course.goalTextA : this.course.goalTextB;
+    },
+    increment(num) {
+      !num && (num = 1);
+      this.counter += num;
+    },
+    decrement() {
+      this.counter > 0 && this.counter--;
+    },
+    setName({ target }, lastName,) {
+      this.me.name = `${target.value} ${lastName}`;
     }
   }
 });
