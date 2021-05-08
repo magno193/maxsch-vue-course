a13929372e5e5ed1c02eb9f264a7bef1ab9dce43
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-07 23:52:13 -0300

    Conditionals: v-if, v-else, v-show

--- 03-conditionals-lists/app.js
+++ 03-conditionals-lists/app.js
@@ -1,7 +1,14 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return { goalInputValue: '', goals: [] };
   },
+  methods: {
+    addGoal() {
+      if (!this.goalInputValue) return;
+      this.goals.push(this.goalInputValue);
+      this.goalInputValue = '';
+    }
+  }
 });
 
 app.mount('#user-goals');
