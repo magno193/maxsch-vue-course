a297e70cefd1644aaf802b1615648c71a796ca61
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-09 08:26:11 -0300

    Lifecycle quick example

--- 04-lifecycle/app.js
+++ 04-lifecycle/app.js
@@ -14,9 +14,36 @@ const app = Vue.createApp({
       this.message = this.$refs.userText.value;
     },
   },
+  beforeCreate() {
+    console.log('beforeCreate() → (nothing on the screen)');
+  },
+  created() {
+    console.log('created() → (nothing on the screen)');
+  },
+  beforeMount() {
+    console.log('beforeMount() → (nothing on the screen)');
+  },
+  mounted() {
+    console.log(`mount() → (It's on the screen)`);
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate() → (Data is updated)');
+  },
+  updated() {
+    console.log('updated() → (Data is updated and on the screen)');
+  },
+  beforeUnmount() {
+    console.log('beforeUnmount() → (only called when app.unmount())');
+  },
+  unmounted() {
+    console.log('unmounted() → (only called when app.unmount())');
+  }
 });
 
 app.mount('#app');
+setTimeout(() => {
+  app.unmount('#app');
+}, 5000);
 // /* ----------------------------------------------------------------------------- */
 // const data = { message: 'Hello', longMessage: 'Hello! World!' };
 // const handler = {
