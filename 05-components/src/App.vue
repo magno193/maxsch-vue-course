2bf8806c52579e57344a631cfb93dd9382a72c79
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-09 11:33:52 -0300

    Creating App component

---
+++ 05-components/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<section>
+  <h2>Friends list</h2>
+  <ul>
+    <li v-for="friend in friends" :key="friend.id">
+      {{friend.name}} - {{friend.email}} - {{friend.phone}}
+    </li>  
+  </ul>
+</section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'john',
+          name: 'John doe',
+          phone: '01 2345 6789',
+          email: 'johndoe@localhost.com',
+        },
+        {
+          id: 'doe',
+          name: 'Doe John',
+          phone: '98 7654 3210',
+          email: 'doejohn@localhost.com',
+        },
+      ]
+    };
+  },
+};
+</script>
