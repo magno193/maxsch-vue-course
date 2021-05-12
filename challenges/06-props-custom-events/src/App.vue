50d0e32cb58d39d5a85dbae40b37181d753659d9
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-12 13:34:04 -0300

    challenge 06: props custom events challenge done

---
+++ challenges/06-props-custom-events/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <section class="container">
+      <ActiveUser :username="user.name" :userage="user.age" />
+      <UserData @submit-user="submitUser" />
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: 'No name, register one',
+        age: 0
+      }
+    };
+  },
+  methods: {
+    submitUser(userData) {
+      this.user.name = userData.name;
+      this.user.age = +userData.age;
+    }
+  }
+}
+</script>
+
+<style>
+#app .container {
+  display: flex;
+  margin: 0 auto;
+  max-width: 780px;
+  justify-content: space-evenly;
+  border: 1px #ccc solid;
+  border-radius: 8px;
+}
+
+#app form {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  margin: 2rem 0;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+}
+</style>
---
+++ challenges/06-props-custom-events/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="submitUser">
+    <input type="text" v-model="nameValue" placeholder="Your name">
+    <input type="number" v-model="ageValue" placeholder="Your age">
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nameValue: '',
+      ageValue: '',
+      userData: {
+        name: '',
+        age: ''
+      }
+    }
+  },
+  methods: {
+    submitUser(){
+      if (this.nameValue && this.ageValue) {
+        this.userData = {
+          name: this.nameValue,
+          age: this.ageValue
+        };
+        this.$emit('submit-user', this.userData);
+      }
+    }
+  },
+  emits: ['submit-user']
+}
+</script>
