00907e33475ff67b22c458f6ff76c9107b4748e7
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-16 02:07:28 -0300

    Dynamic components + keep alive

--- 07-mastering-components/src/App.vue
+++ 07-mastering-components/src/App.vue
@@ -1,34 +1,40 @@
 <template>
   <div>
     <TheHeader />
-    <BadgeList />
-    <UserInfo
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></UserInfo>
-    <CourseGoals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-    </CourseGoals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from "./components/TheHeader.vue";
+import ManageGoals from "./components/ManageGoals.vue";
+import ActiveGoals from "./components/ActiveGoals.vue";
+
 export default {
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
+      selectedComponent: "active-goals",
     };
   },
-  components: { TheHeader, BadgeList, UserInfo, CourseGoals }
+  methods: {
+    setSelectedComponent(component) {
+      this.selectedComponent = component;
+    },
+  },
+  components: {
+    TheHeader,
+    ActiveGoals,
+    ManageGoals,
+  },
 };
 </script>
 
