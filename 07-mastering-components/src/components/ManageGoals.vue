5e411e992be87ce3293d3225d181d5cb158c126f
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-17 14:59:00 -0300

    Applying slot and dialog concepts

--- 07-mastering-components/src/components/ManageGoals.vue
+++ 07-mastering-components/src/components/ManageGoals.vue
@@ -1,4 +1,33 @@
 <template>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal" />
+  <button @click="setGoal">Set Goal</button>
+  <ErrorAlert v-if="inputIsInvalid">
+    <h2>Input is Invalid</h2>
+    <p>Please enter at least a few characters...</p>
+    <button @click="confirmError">Okay</button>
+  </ErrorAlert>
 </template>
+<script>
+import ErrorAlert from "./ErrorAlert";
+export default {
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const inputValue = this.$refs.goal.value;
+      console.log(this.$refs);
+      if (!inputValue) {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+  components: { ErrorAlert },
+};
+</script>
