c6daf21fe8cdcfdb8fd4ffc8dd96aae634ac952e
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-17 17:43:10 -0300

    Base components (Card, Button), Layout component (TheHeader)

--- projects/02-course-project/src/App.vue
+++ projects/02-course-project/src/App.vue
@@ -1,11 +1,13 @@
 <template>
+  <TheHeader title="RememberMe" />
   <StoredResources :resources="storedResources" />
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources';
+import TheHeader from './components/layouts/TheHeader';
 export default {
-  components: { StoredResources },
+  components: { StoredResources, TheHeader },
   data() {
     return {
       storedResources: [
