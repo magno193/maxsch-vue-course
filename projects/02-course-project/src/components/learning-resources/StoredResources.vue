c58431d1da1a1725ecfc5b2c17809d257e117813
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-17 17:19:17 -0300

    First components and props

---
+++ projects/02-course-project/src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <LearningResource
+      v-for="resource in resources"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    />
+  </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource';
+export default {
+  components: { LearningResource },
+  props: ['resources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
