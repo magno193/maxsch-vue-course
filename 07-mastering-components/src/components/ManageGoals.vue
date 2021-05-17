3e680867fc9d5279a4e1629107b5f3ba278e45fb
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-17 15:02:42 -0300

    Fixing semantic with built-in Vue teleport

--- 07-mastering-components/src/components/ManageGoals.vue
+++ 07-mastering-components/src/components/ManageGoals.vue
@@ -2,11 +2,13 @@
   <h2>Manage Goals</h2>
   <input type="text" ref="goal" />
   <button @click="setGoal">Set Goal</button>
-  <ErrorAlert v-if="inputIsInvalid">
-    <h2>Input is Invalid</h2>
-    <p>Please enter at least a few characters...</p>
-    <button @click="confirmError">Okay</button>
-  </ErrorAlert>
+  <teleport to="body">
+    <ErrorAlert v-if="inputIsInvalid">
+      <h2>Input is Invalid</h2>
+      <p>Please enter at least a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </ErrorAlert>
+  </teleport>
 </template>
 <script>
 import ErrorAlert from "./ErrorAlert";
