40ebf32da325dd2989247af944f12898901a32bd
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-19 16:23:34 -0300

    fetching data from firebase + fetching when mounted

--- 09-http-requests/src/components/survey/UserExperiences.vue
+++ 09-http-requests/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">
+          Load Submitted Experiences
+        </base-button>
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,31 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch('https://maxsch-vue-http-default-rtdb.firebaseio.com/surveys.json')
+        .then((response) => {
+          if (response.ok) return response.json();
+        })
+        .then((data) => {
+          const results = [];
+          for (const id in data) {
+            results.push({ id, name: data[id].name, rating: data[id].rating });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
   },
 };
 </script>
