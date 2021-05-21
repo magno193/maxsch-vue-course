57d80e38b8a4cbb4295aaa52e859db00734900ea
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-20 21:29:50 -0300

    Using route params as props, redirect, alias and catching all routes

--- 10-routing/src/components/teams/TeamMembers.vue
+++ 10-routing/src/components/teams/TeamMembers.vue
@@ -18,6 +18,7 @@ import UserItem from '../users/UserItem.vue';
 
 export default {
   inject: ['users', 'teams'],
+  props: ['teamId'],
   components: {
     UserItem,
   },
@@ -28,8 +29,7 @@ export default {
     };
   },
   methods: {
-    loadTeamMembers(route) {
-      const { teamId } = route.params;
+    loadTeamMembers(teamId) {
       const selectedTeam = this.teams.find((team) => team.id === teamId);
       if (!selectedTeam) return;
       const members = selectedTeam.members;
@@ -43,11 +43,11 @@ export default {
     },
   },
   created() {
-    this.loadTeamMembers(this.$route);
+    this.loadTeamMembers(this.teamId);
   },
   watch: {
-    $route(newRoute) {
-      this.loadTeamMembers(newRoute);
+    teamId(newId) {
+      this.loadTeamMembers(newId);
     },
   },
 };
