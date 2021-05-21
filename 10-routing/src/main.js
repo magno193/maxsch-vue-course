cd4920a73452350e08802112f625ec30fc6576ea
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-21 11:50:39 -0300

    Query params and multiple same level router-view example

--- 10-routing/src/main.js
+++ 10-routing/src/main.js
@@ -4,7 +4,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
+import UsersFooter from './components/users/UsersFooter';
 import TeamMembers from './components/teams/TeamMembers';
+import TeamsFooter from './components/teams/TeamsFooter';
 import NotFound from './components/NotFound';
 
 const router = createRouter({
@@ -14,12 +16,12 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
       ]
     },
-    { path: '/users', component: UsersList, alias: '/user' },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter }, alias: '/user' },
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'router-active'
