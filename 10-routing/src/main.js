4e48e674fe828ca6ae1116641e641d3035eb1ab0
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-22 23:14:24 -0300

    Reorganizing route files

--- 10-routing/src/main.js
+++ 10-routing/src/main.js
@@ -1,62 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-
+import router from './router';
 import App from './App.vue';
-import TeamsList from './pages/TeamsList';
-import UsersList from './pages/UsersList';
-import UsersFooter from './pages/UsersFooter';
-import TeamMembers from './components/teams/TeamMembers';
-import TeamsFooter from './pages/TeamsFooter';
-import NotFound from './pages/NotFound';
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      meta: { needsAuth: true },
-      components: { default: TeamsList, footer: TeamsFooter },
-      children: [
-        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
-      ]
-    },
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      alias: '/user',
-      beforeEnter(to, from, next) {
-        console.log('Users beforeEnter');
-        next();
-      }
-    },
-    { path: '/:notFound(.*)', component: NotFound }
-  ],
-  linkActiveClass: 'router-active',
-  scrollBehavior(_, _2, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  }
-});
-
-// Navigation Guard
-router.beforeEach((to, from, next) => {
-  console.log('Global beforeEach');
-
-  if (to.meta.needsAuth) {
-    console.log('Needs auth!')
-  }
-  next();
-});
-
-router.afterEach((to, from) => {
-  // sending analytics data
-  console.log('Global afterEach');
-  console.log(to, from);
-});
 
 const app = createApp(App);
 app.use(router);
---
+++ 10-routing/src/router.js
@@ -0,0 +1,58 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import TeamsList from './pages/TeamsList';
+import UsersList from './pages/UsersList';
+import UsersFooter from './pages/UsersFooter';
+import TeamMembers from './components/teams/TeamMembers';
+import TeamsFooter from './pages/TeamsFooter';
+import NotFound from './pages/NotFound';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/teams' },
+    {
+      name: 'teams',
+      path: '/teams',
+      meta: { needsAuth: true },
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
+      ]
+    },
+    {
+      path: '/users',
+      components: { default: UsersList, footer: UsersFooter },
+      alias: '/user',
+      beforeEnter(to, from, next) {
+        console.log('Users beforeEnter');
+        next();
+      }
+    },
+    { path: '/:notFound(.*)', component: NotFound }
+  ],
+  linkActiveClass: 'router-active',
+  scrollBehavior(_, _2, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  }
+});
+
+// Navigation Guard
+router.beforeEach((to, from, next) => {
+  console.log('Global beforeEach');
+
+  if (to.meta.needsAuth) {
+    console.log('Needs auth!')
+  }
+  next();
+});
+
+router.afterEach((to, from) => {
+  // sending analytics data
+  console.log('Global afterEach');
+  console.log(to, from);
+});
+
+export default router;
