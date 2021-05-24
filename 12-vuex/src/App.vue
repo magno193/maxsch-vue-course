c1dee9a97aee0db53a65e752ff94d74d3406a1c3
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-24 11:41:07 -0300

    Vuex: using state example

--- 12-vuex/src/App.vue
+++ 12-vuex/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <TheCounter />
+    <button @click="addOne">Add 1</button>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
-
+import TheCounter from './components/TheCounter';
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  methods: {
+    addOne() {
+      this.$store.state.counter++;
+    },
   },
 };
 </script>
---
+++ 12-vuex/src/components/TheCounter.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>{{ $store.state.counter }}</h3>
+</template>
+
+<script>
+export default {
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+};
+</script>
