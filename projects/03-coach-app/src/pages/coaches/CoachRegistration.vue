4e3ae6a020d2f490a104e6430a2d2d71b9edd11e
Alexandre Ferreira <alexmlf93@gmail.com>
2021-05-28 13:45:48 -0300

    Coach registration, form component

--- projects/03-coach-app/src/pages/coaches/CoachRegistration.vue
+++ projects/03-coach-app/src/pages/coaches/CoachRegistration.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>Coach Registration</div>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: { CoachForm },
+};
+</script>
