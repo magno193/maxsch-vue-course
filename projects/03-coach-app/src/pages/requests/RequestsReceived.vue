2582d50532c53897cf57d039d38fb915ae5560bf
Alexandre Ferreira <alexmlf93@gmail.com>
2021-06-01 02:17:06 -0300

    Listing incoming requests with vuex/getters

--- projects/03-coach-app/src/pages/requests/RequestsReceived.vue
+++ projects/03-coach-app/src/pages/requests/RequestsReceived.vue
@@ -1,3 +1,50 @@
 <template>
-  <div>Requests Received</div>
+  <section>
+    <base-card>
+      <header>
+        <h2>Requests received</h2>
+      </header>
+      <ul v-if="hasRequests">
+        <request-item
+          v-for="request in requests"
+          :key="request.id"
+          :email="request.userEmail"
+          :message="request.userMessage"
+        />
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
+    </base-card>
+  </section>
 </template>
+
+<script>
+import { mapGetters } from 'vuex';
+import RequestItem from '../../components/requests/RequestItem';
+export default {
+  name: 'RequestsReceived',
+  components: { RequestItem },
+  computed: {
+    ...mapGetters('requests', ['requests', 'hasRequests']),
+    // requests() {
+    //   return this.$store.getters['requests/requests'];
+    // },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
