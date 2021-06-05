cec2a70e9c6d8e1b9e78ff9a4cd23ff2be2f1771
Alexandre Ferreira <alexmlf93@gmail.com>
2021-06-05 03:56:17 -0300

    Using http requests with fetch for PUT and GET

--- projects/03-coach-app/src/store/modules/coaches/actions.js
+++ projects/03-coach-app/src/store/modules/coaches/actions.js
@@ -1,14 +1,48 @@
 export default {
-  registerCoach(context, payload) {
+  async registerCoach(context, payload) {
+    const userId = context.rootGetters.userId
     const { first, last, desc, rate, areas } = payload;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: first,
       lastName: last,
       description: desc,
       hourlyRate: rate,
       areas,
     };
-    context.commit('registerCoach', coachData);
+
+    const response = await fetch(`https://coach-app-2f095-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    if (response.ok) {
+      console.table(coachData)
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
+  },
+  async loadCoaches(context) {
+    const response = await fetch('https://coach-app-2f095-default-rtdb.firebaseio.com/coaches.json');
+    const responseData = await response.json();
+
+    if (response.ok) {
+      // ... 
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   }
 };
