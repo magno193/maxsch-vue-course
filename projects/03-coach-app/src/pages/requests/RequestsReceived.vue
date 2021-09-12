228716a4671cc70744681141fb33d94ed09a4c95
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-12 16:24:13 -0300

    Feature: carregando requests http

--- projects/03-coach-app/src/pages/requests/RequestsReceived.vue
+++ projects/03-coach-app/src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,20 @@
 <template>
+  <base-dialog
+    :show="!!errorMessage"
+    title="An error has occurred"
+    @close="handleError"
+  >
+    {{ errorMessage }}
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests received</h2>
       </header>
-      <ul v-if="hasRequests">
+      <div v-if="isLoading">
+        <base-spinner />
+      </div>
+      <ul v-else-if="hasRequests && !isLoading">
         <request-item
           v-for="request in requests"
           :key="request.id"
@@ -25,6 +35,28 @@ export default {
   components: { RequestItem },
   computed: {
     ...mapGetters('requests', ['requests', 'hasRequests']),
+  },
+  data: () => ({
+    isLoading: true,
+    errorMessage: null,
+  }),
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.errorMessage =
+          error.message || 'Something failed. Please try again later.';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.errorMessage = null;
+    },
+  },
+  created() {
+    this.loadRequests();
   },
 };
 </script>
--- projects/03-coach-app/src/store/modules/requests/actions.js
+++ projects/03-coach-app/src/store/modules/requests/actions.js
@@ -21,5 +21,29 @@ export default {
     newRequest.coachId = coachId;
 
     commit('addRequest', newRequest);
+
   },
+  async fetchRequests({ rootGetters, commit }) {
+    const coachId = rootGetters.userId
+    const response = await fetch(`https://coach-app-2f095-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch requests.');
+      throw error;
+    }
+
+    const requests = [];
+    for (const key in responseData) {
+      const request = {
+        id: key,
+        coachId,
+        userEmail: responseData[key].userEmail,
+        userMessage: responseData[key].userMessage,
+      }
+      requests.push(request);
+    }
+
+    commit('setRequests', requests);
+  }
 };
