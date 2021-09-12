40ea77d348d374afd98492816d40bc9c8e27f160
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-12 15:34:21 -0300

    Feature: http post request para firebase

--- projects/03-coach-app/src/store/modules/requests/actions.js
+++ projects/03-coach-app/src/store/modules/requests/actions.js
@@ -1,12 +1,25 @@
 export default {
-  contactCoach({ commit }, payload) {
+  async contactCoach({ commit }, payload) {
     const { coachId, email, message } = payload;
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: coachId,
       userEmail: email,
       userMessage: message,
     };
+
+    const response = await fetch(`https://coach-app-2f095-default-rtdb.firebaseio.com/requests/${coachId}.json`, {
+      method: 'POST',
+      body: JSON.stringify(newRequest)
+    })
+    const responseData = response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send request.');
+      throw error;
+    }
+
+    newRequest.id = responseData.name; // name é id gerado automaticamente pelo o firebase
+    newRequest.coachId = coachId;
+
     commit('addRequest', newRequest);
   },
 };
