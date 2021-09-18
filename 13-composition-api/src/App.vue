63679366bba5f7cb41c4fc2acc14a79e01b5b93b
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-18 16:15:44 -0300

    Usando ref como objeto

--- 13-composition-api/src/App.vue
+++ 13-composition-api/src/App.vue
@@ -1,15 +1,45 @@
 <template>
   <section class="container">
     <h2>{{ userName }}</h2>
+    <h3>{{ age }}</h3>
+  </section>
+  <section class="container">
+    <h2>Como objeto</h2>
+    <h3>{{ user.name }}</h3>
+    <h3>{{ user.age }}</h3>
   </section>
 </template>
 
 <script>
+import { ref } from 'vue';
 export default {
-  data() {
-    return {
-      userName: 'Maximilian',
+  setup() {
+    // ref é para um valor reativo
+    const uName = ref('Alexandre');
+    const uAge = ref('Idade');
+
+    const user = ref({
+      name: 'Alexandre',
+      age: null,
+    });
+
+    const setAge = (year, month, day) => {
+      return (
+        new Date(
+          Date.now() - new Date(year, month - 1, day).getTime()
+        ).getUTCFullYear() - 1970
+      );
     };
+    setTimeout(() => {
+      // .value é onde modificamos o valor reativo
+      uName.value += ' Magno';
+      uAge.value = setAge(1993, 12, 6);
+
+      user.value.name += ' Magno';
+      user.value.age = setAge(1993, 12, 6);
+    }, 2000);
+
+    return { userName: uName, age: uAge, user };
   },
 };
 </script>
