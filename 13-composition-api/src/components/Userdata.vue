7eaa27b646b7db8b8a6ea171c9f00f0f8acefadc
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-19 03:04:34 -0300

    Composition API: inject/provide e lifecycle hooks

--- 13-composition-api/src/components/Userdata.vue
+++ 13-composition-api/src/components/Userdata.vue
@@ -5,13 +5,42 @@
 </template>
 
 <script>
-import { computed } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
 export default {
-  props: ['firstName', 'lastName', 'age'],
+  props: ['firstName', 'lastName'],
   setup(props, context) {
     const fullName = computed(() => `${props.firstName} ${props.lastName}`);
     console.log(context);
-    return { fullName };
+    const age = inject('userAge');
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount');
+    });
+    onMounted(() => {
+      console.log('onMounted');
+    });
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(() => {
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(() => {
+      console.log('onUnmounted');
+    });
+    return { fullName, age };
   },
 };
 </script>
