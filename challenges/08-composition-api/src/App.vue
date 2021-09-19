ef748bdab9433cef886c702fa750e5a87a0193ce
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-18 22:56:23 -0300

    Desafio completado

--- challenges/08-composition-api/src/App.vue
+++ challenges/08-composition-api/src/App.vue
@@ -2,9 +2,16 @@
   <h2>My Course Goal</h2>
   <!-- Task 1: Output your main course goal with help of the composition API -->
   <!-- Don't hardcode it into the template, instead hardcode it into the JS code -->
-  <h3>OUTPUT COURSE GOAL</h3>
+  <h3 v-show="showGoal">{{ courseGoal }}</h3>
   <!-- Task 2: Toggle (show/ hide) the goal with help of the button  -->
-  <button>Toggle Goal</button>
+  <button @click="toggleGoal">Toggle goal</button>
+
+  <button @click="goal.isVisible = !goal.isVisible">Toggle another goal</button>
+  <h3 v-show="goal.isVisible">{{ goal.message }}</h3>
+  <hr />
+  <h2>Options API</h2>
+  <button @click="optGoal.isVisible = !optGoal.isVisible">Option Toggle</button>
+  <h3 v-show="optGoal.isVisible">{{ optGoal.message }}</h3>
   <!-- Task 3: Manage data in three ways -->
   <!-- => Separate refs -->
   <!-- => Ref Object -->
@@ -13,9 +20,28 @@
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
 export default {
-  
-}
+  data: () => ({
+    optGoal: {
+      message: 'Option goal completed!',
+      isVisible: false,
+    },
+  }),
+  setup() {
+    const courseGoal = ref('Termine o curso!');
+    const showGoal = ref(false);
+    const toggleGoal = () => {
+      showGoal.value = !showGoal.value;
+    };
+
+    const goal = reactive({
+      message: 'You have completed your course goal!',
+      isVisible: false,
+    });
+    return { courseGoal, showGoal, toggleGoal, goal };
+  },
+};
 </script>
 
 <style>
