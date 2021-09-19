68359789d9811f8a2e7b7c10230ba0b9ec95d7ee
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-19 17:52:19 -0300

    Usando roteamento com props em composition API

--- projects/05-products-manager/src/pages/ProductDetails.vue
+++ projects/05-products-manager/src/pages/ProductDetails.vue
@@ -1,19 +1,25 @@
 <template>
   <section>
     <h2>{{ title }}</h2>
-    <h3>${{ price}}</h3>
-    <p>{{ description}}</p>
+    <h3>${{ price }}</h3>
+    <p>{{ description }}</p>
   </section>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { inject, computed } from 'vue';
 
 export default {
-  setup() {
-    const title = ref('');
-    const price = ref(null);
-    const description = ref('');
+  props: ['pid'],
+  setup(props) {
+    const products = inject('products');
+
+    const selectedProduct = computed(() =>
+      products.value.find((prdt) => prdt.id === props.pid)
+    );
+    const title = selectedProduct.value.title;
+    const price = selectedProduct.value.price;
+    const description = selectedProduct.value.description;
 
     return { title, price, description };
   },
