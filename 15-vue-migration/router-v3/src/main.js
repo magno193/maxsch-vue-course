a87de5f325e0e7bcfb08447af2989c2a2e7f7336
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-20 23:33:03 -0300

    Comparação: Mudanças de vue-router para vue3

--- 15-vue-migration/router-v3/src/main.js
+++ 15-vue-migration/router-v3/src/main.js
@@ -1,7 +1,22 @@
 import { createApp } from 'vue';
-
+import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
+import UsersList from './pages/UsersList';
+import WelcomeScreen from './pages/WelcomeScreen';
 
-// Add router!
 
-createApp(App).mount('#app');
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: WelcomeScreen },
+    { path: '/users', component: UsersList },
+  ],
+});
+
+const app = createApp(App);
+app.use(router);
+
+// Certificar de inicializar router antes the montar #app
+router.isReady().then(() => {
+  app.mount('#app');
+});
