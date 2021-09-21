a0e32ebd37aa3931e28703effd52dc2db5edbc53
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-20 23:14:47 -0300

    Transição, estilo com classe -from em vue3

--- 15-vue-migration/transitions-v3/app.js
+++ 15-vue-migration/transitions-v3/app.js
@@ -1,11 +1,10 @@
-new Vue({
-  el: '#app',
-  data: {
+const app = Vue.createApp({
+  data: () => ({
     isVisible: true
-  },
+  }),
   methods: {
     toggleBox() {
       this.isVisible = !this.isVisible;
     }
   },
-});
+}).mount('#app');
